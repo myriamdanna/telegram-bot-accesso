2baa2503cd79fd2e7bc6b5151486989d7b196a57
myriamdanna <privatemyriam@gmail.com>
2026-04-28 22:59:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ bot.on("message", async (msg) => {
   
       client_reference_id: chatId,
 
-      customer_creation: 'always', 
-
       customer_data: {
         metadata: {
           telegramId: chatId.toString(),
